9543e3eec0b984860ba0c4273984b3544e9196df
johnnysta <janos.stampf@gmail.com>
2024-04-05 13:12:34 +0200

    Unused components removed + UI changes

--- frontend-angular/src/app/app-routing.module.ts
+++ frontend-angular/src/app/app-routing.module.ts
@@ -2,10 +2,6 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {ContactsListComponent} from "./components/contacts-list/contacts-list.component";
 import {LoginComponent} from "./components/login/login.component";
-import {ContactFormComponent} from "./components/contact-form/contact-form.component";
-import {ContactDetailsComponent} from "./components/contact-details/contact-details.component";
-import {PhoneFormComponent} from "./components/phone-form/phone-form.component";
-import {AddressFromComponent} from "./components/address-from/address-from.component";
 import {UserRegistrationComponent} from "./components/user-registration/user-registration.component";
 import {HomeComponent} from "./components/home/home.component";
 import {ContactFormFullComponent} from "./components/contact-form-full/contact-form-full.component";
@@ -16,13 +12,8 @@ const routes: Routes = [
   {path: "login", component: LoginComponent},
   {path: "registerUser", component: UserRegistrationComponent},
   {path: "contacts", component: ContactsListComponent},
-  {path: "contactsForm", component: ContactFormComponent},
-  {path: "contactsForm/:id", component: ContactFormComponent},
-  {path: "contactDetails/:id", component: ContactDetailsComponent},
-  {path: "phoneForm/:id", component: PhoneFormComponent},
   {path: "phoneFormLocal/:id", component: PhoneFormLocalComponent},
   {path: "phoneFormLocal", component: PhoneFormLocalComponent},
-  {path: "addressForm/:id", component: AddressFromComponent},
   {path: "addressFormLocal", component: AddressFormLocalComponent},
   {path: "addressFormLocal/:id", component: AddressFormLocalComponent},
   {path: "contactsFormFull/:id", component: ContactFormFullComponent},
--- frontend-angular/src/app/app.module.ts
+++ frontend-angular/src/app/app.module.ts
@@ -1,28 +1,21 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { ContactsListComponent } from './components/contacts-list/contacts-list.component';
-import { LoginComponent } from './components/login/login.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {NavbarComponent} from './components/navbar/navbar.component';
+import {ContactsListComponent} from './components/contacts-list/contacts-list.component';
+import {LoginComponent} from './components/login/login.component';
 import {ReactiveFormsModule} from "@angular/forms";
 import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
-import { ContactFormComponent } from './components/contact-form/contact-form.component';
-import { PhonesListComponent } from './components/phones-list/phones-list.component';
-import { AddressesListComponent } from './components/addresses-list/addresses-list.component';
-import { ContactDetailsComponent } from './components/contact-details/contact-details.component';
-import { PhoneFormComponent } from './components/phone-form/phone-form.component';
-import { AddressFromComponent } from './components/address-from/address-from.component';
-import { ContactBasicDataComponent } from './components/contact-basic-data/contact-basic-data.component';
-import { UserRegistrationComponent } from './components/user-registration/user-registration.component';
+import {UserRegistrationComponent} from './components/user-registration/user-registration.component';
 import {AuthInterceptor} from "./utils/auth-interceptor.service";
-import { HomeComponent } from './components/home/home.component';
-import { ContactFormFullComponent } from './components/contact-form-full/contact-form-full.component';
-import { PhonesListSubComponent } from './components/phones-list-sub/phones-list-sub.component';
-import { PhoneFormLocalComponent } from './components/phone-form-local/phone-form-local.component';
-import { AddressesListSubComponent } from './components/addresses-list-sub/addresses-list-sub.component';
-import { AddressFormLocalComponent } from './components/address-form-local/address-form-local.component';
+import {HomeComponent} from './components/home/home.component';
+import {ContactFormFullComponent} from './components/contact-form-full/contact-form-full.component';
+import {PhonesListSubComponent} from './components/phones-list-sub/phones-list-sub.component';
+import {PhoneFormLocalComponent} from './components/phone-form-local/phone-form-local.component';
+import {AddressesListSubComponent} from './components/addresses-list-sub/addresses-list-sub.component';
+import {AddressFormLocalComponent} from './components/address-form-local/address-form-local.component';
 
 @NgModule({
   declarations: [
@@ -30,13 +23,6 @@ import { AddressFormLocalComponent } from './components/address-form-local/addre
     NavbarComponent,
     ContactsListComponent,
     LoginComponent,
-    ContactFormComponent,
-    PhonesListComponent,
-    AddressesListComponent,
-    ContactDetailsComponent,
-    PhoneFormComponent,
-    AddressFromComponent,
-    ContactBasicDataComponent,
     UserRegistrationComponent,
     HomeComponent,
     ContactFormFullComponent,
@@ -51,7 +37,8 @@ import { AddressFormLocalComponent } from './components/address-form-local/addre
     ReactiveFormsModule,
     HttpClientModule
   ],
-  providers: [{ provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }],
+  providers: [{provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
