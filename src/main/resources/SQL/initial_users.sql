81a79bccf1b8d032b7caad468ae2ee7c295d9d25
johnnysta <janos.stampf@gmail.com>
2024-05-07 11:51:02 +0200

    sql script for initial users added

---
+++ src/main/resources/SQL/initial_users.sql
@@ -0,0 +1,3 @@
+insert into app_user (email, first_name, last_name, password, role) values ('admin@a.hu', 'Béla', 'Admin', '$2a$10$b3BFGeE4wp.jvUOIUQ/S/eq.R.giheIOSHmOJ1oTwuF2ZqTv0zONi', 'ADMIN');
+insert into app_user (email, first_name, last_name, password, role) values ('dezso@a.hu', 'Dezső', 'User', '$2a$10$b3BFGeE4wp.jvUOIUQ/S/eq.R.giheIOSHmOJ1oTwuF2ZqTv0zONi', 'ADMIN');
+insert into app_user (email, first_name, last_name, password, role) values ('gizi@a.hu', 'Gizi', 'UserAdmin', '$2a$10$b3BFGeE4wp.jvUOIUQ/S/eq.R.giheIOSHmOJ1oTwuF2ZqTv0zONi', 'ADMIN');
