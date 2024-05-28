7a0e5a6b07d8ee4db472cb74b16398b67071c698
johnnysta <janos.stampf@gmail.com>
2024-05-28 10:56:18 +0200

    Organizing environment variables for deployment/local run

--- src/main/java/com/example/contactsapplication/ContactsApplication.java
+++ src/main/java/com/example/contactsapplication/ContactsApplication.java
@@ -2,8 +2,14 @@ package com.example.contactsapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
+@PropertySources({
+        @PropertySource("classpath:application.properties"),
+        @PropertySource("classpath:.env")
+})
 public class ContactsApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/example/contactsapplication/config/DotenvConfig.java
@@ -0,0 +1,20 @@
+package com.example.contactsapplication.config;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DotenvConfig {
+
+    @Bean
+    public Dotenv dotenv() {
+        return Dotenv.configure().ignoreIfMissing().load();
+    }
+
+
+//    @Bean
+//    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+//        return new PropertySourcesPlaceholderConfigurer();
+//    }
+}
