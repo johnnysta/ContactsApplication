7273dd1e3dd35a7b6e2a7ba3b337aab2dde952b2
johnnysta <janos.stampf@gmail.com>
2024-05-30 17:11:21 +0200

    sql files for demo data added

---
+++ src/main/resources/SQL/app_user.sql
@@ -0,0 +1,7 @@
+insert into app_user (first_name, last_name, email, password, role) values ('Glory', 'Cicullo', 'gcicullo3@berkeley.edu', '$2a$10$b3BFGeE4wp.jvUOIUQ/S/eq.R.giheIOSHmOJ1oTwuF2ZqTv0zONi', 'USER');
+insert into app_user (first_name, last_name, email, password, role) values ('Zitella', 'Readitt', 'zreaditt4@e-recht24.de', '$2a$10$b3BFGeE4wp.jvUOIUQ/S/eq.R.giheIOSHmOJ1oTwuF2ZqTv0zONi', 'USER');
+insert into app_user (first_name, last_name, email, password, role) values ('Janette', 'Bess', 'jbess5@tumblr.com', '$2a$10$b3BFGeE4wp.jvUOIUQ/S/eq.R.giheIOSHmOJ1oTwuF2ZqTv0zONi', 'USER');
+insert into app_user (first_name, last_name, email, password, role) values ('Glad', 'Keirle', 'gkeirle6@foxnews.com', '$2a$10$b3BFGeE4wp.jvUOIUQ/S/eq.R.giheIOSHmOJ1oTwuF2ZqTv0zONi', 'USER');
+insert into app_user (first_name, last_name, email, password, role) values ('Emalee', 'Mabbott', 'emabbott7@creativecommons.org', '$2a$10$b3BFGeE4wp.jvUOIUQ/S/eq.R.giheIOSHmOJ1oTwuF2ZqTv0zONi', 'USER');
+insert into app_user (first_name, last_name, email, password, role) values ('Lindsy', 'Tirte', 'ltirte8@dell.com', '$2a$10$b3BFGeE4wp.jvUOIUQ/S/eq.R.giheIOSHmOJ1oTwuF2ZqTv0zONi', 'USER');
+insert into app_user (first_name, last_name, email, password, role) values ('Lorie', 'Clipsham', 'lclipsham9@nyu.edu', '$2a$10$b3BFGeE4wp.jvUOIUQ/S/eq.R.giheIOSHmOJ1oTwuF2ZqTv0zONi', 'USER');
